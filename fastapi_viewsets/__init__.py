937ec0e2bc7895bf59fd71c5a026384372cb1e8a
svalench <chitsalex@gmail.com>
2022-05-09 23:14:26 +0300

    review and added protect endpoints

--- fastapi_viewsets/__init__.py
+++ fastapi_viewsets/__init__.py
@@ -1,8 +1,10 @@
+import functools
 import os
 from collections import Iterable
 from typing import List, Optional, Any, Callable, Type
 
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 
 from fastapi_viewsets.constants import ALLOWED_METHODS, MAP_METHODS
@@ -27,7 +29,10 @@ class BaseViewset(APIRouter):
         self.model = model
         self.db_session = db_session
 
-    def register(self, methods: List = None):
+    def register(self, methods: List = None, oauth_protect: OAuth2PasswordBearer = None,
+                 protected_methods: List = None):
+        if not protected_methods:
+            protected_methods = []
         if not methods:
             methods = self.ALLOWED_METHODS
         if not isinstance(methods, Iterable):
@@ -39,29 +44,35 @@ class BaseViewset(APIRouter):
                 self_method.__annotations__['item'] = self.response_model
             except:
                 pass
+            old_docstring = self_method.__doc__
+            if method in protected_methods and oauth_protect:
+                self_method = functools.partial(self_method, token=Depends(oauth_protect))
+                self_method.__doc__ = old_docstring
             self.add_api_route(
                 self.endpoint + data_method.get('path', ''),
                 self_method,
-                response_model=List[self.response_model],
+                response_model=List[self.response_model] if data_method.get('is_list', False) else self.response_model,
                 tags=self.tags,
                 methods=[data_method.get('http_method')],
             )
 
     def list(self, limit: Optional[int] = 10,
              offset: Optional[int] = 0,
-             search: Optional[str] = None):
+             search: Optional[str] = None,
+             token: str = ''
+             ):
         return get_list_queryset(self.model, db_session=self.db_session, limit=limit, offset=offset)
 
-    def get_element(self, id: Any):
-        return get_element_by_id(self.model, db_session=self.db_session, id=id)
+    def get_element(self, id: Any, token: str = ''):
+        return get_element_by_id(self.model, db_session=self.db_session, id=id, )
 
-    def create_element(self, item=Body(...)):
-        return create_element(self.model, db_session=self.db_session, data=item)
+    def create_element(self, item=Body(...), token: str = ''):
+        return create_element(self.model, db_session=self.db_session, data=dict(item))
 
-    def update_element(self, id: str, item=Body(...)):
-        return update_element(self.model, self.db_session, id, item)
+    def update_element(self, id: str, item=Body(...), token: str = ''):
+        return update_element(self.model, self.db_session, id, dict(item))
 
-    def delete_element(self, id: str):
+    def delete_element(self, id: str, token: str = ''):
         return {'status': True, 'text': "успешно удалено"} if delete_element(self.model, self.db_session,
                                                                              id) is True else {'status': False,
                                                                                                'text': "не удалено"}
--- fastapi_viewsets/constants.py
+++ fastapi_viewsets/constants.py
@@ -4,12 +4,12 @@ SEPARATOR = os.path.sep
 BASE_DIR = os.getcwd()
 BASE_DIR += SEPARATOR
 
-MAP_METHODS = {'GET': {'method': 'get_element', 'http_method': 'GET', 'path': '/{id}'},
-               'POST': {'method': 'create_element', 'http_method': 'POST', 'path': ''},
-               'PUT': {'method': 'update_element', 'http_method': 'PUT', 'path': ''},
-               'PATCH': {'method': 'update_element', 'http_method': 'PATCH', 'path': ''},
-               'DELETE': {'method': 'delete_element', 'http_method': 'DELETE', 'path': ''},
-               'LIST': {'method': 'list', 'http_method': 'GET', 'path': ''},
+MAP_METHODS = {'GET': {'method': 'get_element', 'http_method': 'GET', 'path': '/{id}', 'is_list': False},
+               'POST': {'method': 'create_element', 'http_method': 'POST', 'path': '', 'is_list': False},
+               'PUT': {'method': 'update_element', 'http_method': 'PUT', 'path': '', 'is_list': False},
+               'PATCH': {'method': 'update_element', 'http_method': 'PATCH', 'path': '', 'is_list': False},
+               'DELETE': {'method': 'delete_element', 'http_method': 'DELETE', 'path': '', 'is_list': False},
+               'LIST': {'method': 'list', 'http_method': 'GET', 'path': '', 'is_list': True},
                }
 
 ALLOWED_METHODS = [key for key in MAP_METHODS.keys()]
